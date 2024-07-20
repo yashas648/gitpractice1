616d59e9ea70a5e1fd089959d85ae8b6fbecb5d0
yashas <yashasrs64@gmail.com>
2024-07-20 11:55:03 +0530

    ssit function resoplved

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello")
+console.log("recreated");
+let c= 99;
+function gg() {
+    return c<=55;
+}
+let d= 99;
+let cc=99;
+let dd=99;
+function ssit(params) {
+   let b=55
+   console.log("ssit matks is "+b); 
+}
